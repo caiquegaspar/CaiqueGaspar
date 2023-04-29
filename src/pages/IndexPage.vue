f966f70148becef75e59c8d68ab0669027da0cbf
caiquegaspar <caiqsgaspar@hotmail.com>
2023-04-28 21:55:39 -0300

    added progress bar fixed on page top

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useDebounce } from "@composables/useDebounce";
 import { useRange } from "@composables/useRange";
 
@@ -7,10 +7,12 @@ import HomeComponent from "@components/HomeComponent.vue";
 import AboutComponent from "@components/AboutComponent.vue";
 import ServicesComponent from "@components/ServicesComponent.vue";
 
+const progressPercentage = computed(() => (pagePos.value / MAX) * 100);
+
 const directions: { [key: string]: number } = { previous: -100, next: 100 };
 const [MIN, MAX]: number[] = [0, 200];
 
-const pagePos = ref<number>(200);
+const pagePos = ref<number>(0);
 const startTouch = ref<TouchEvent>();
 const endTouch = ref<TouchEvent>();
 
@@ -43,29 +45,43 @@ onMounted(() => {
 </script>
 
 <template>
-  <main :style="`transform: translateY(-${pagePos}vh)`">
-    <HomeComponent
-      class="section_page"
-      @changeSection="changeSection"
-      :isActive="pagePos === 0"
-    />
+  <main>
+    <div class="progress_bar">
+      <span class="bar" :style="`width: ${progressPercentage}%`"></span>
+    </div>
 
-    <AboutComponent
-      class="section_page"
-      @changeSection="changeSection"
-      :isActive="pagePos === 100"
-    />
+    <div class="main_content" :style="`transform: translateY(-${pagePos}vh)`">
+      <HomeComponent
+        class="section_page"
+        @changeSection="changeSection"
+        :isActive="pagePos === 0"
+      />
 
-    <ServicesComponent
-      class="section_page"
-      @changeSection="changeSection"
-      :isActive="pagePos === 200"
-    />
+      <AboutComponent
+        class="section_page"
+        @changeSection="changeSection"
+        :isActive="pagePos === 100"
+      />
+
+      <ServicesComponent
+        class="section_page"
+        @changeSection="changeSection"
+        :isActive="pagePos === 200"
+      />
+    </div>
   </main>
 </template>
 
 <style scoped>
-main {
+.progress_bar {
+  @apply fixed top-0 left-0 z-50 w-full h-1 bg-[#35495e];
+}
+.bar {
+  @apply block h-full bg-[linear-gradient(90deg,#33ffbb,#31acff,#2ad39f,#0170f0)];
+  @apply transition-all duration-700;
+}
+
+.main_content {
   @apply absolute h-screen w-screen font-['Raleway',sans-serif] transition-all duration-500;
 }
 </style>
